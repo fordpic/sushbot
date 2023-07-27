a17d8747baa205f6de47fcc442d350de39638347
Ford Pickert <fordpickert@gmail.com>
2023-07-27 16:32:20 -0500

    get discord token, add to env, load from env, mainnet sush addy

--- src/main.py
+++ src/main.py
@@ -2,3 +2,10 @@ import discord
 from discord.ext import tasks, commands
 import os
 from dotenv import load_dotenv
+
+from utils.price_fetcher import fetch_token_prices
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+MAINNET_SUSHI_ADDRESS = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
