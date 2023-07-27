bb5b8aa12bd7ef1e704ac2ccca6d2e1cc07b32a1
Ford Pickert <fordpickert@gmail.com>
2023-07-27 16:36:20 -0500

    price fetching functionality complete, testing

--- src/main.py
+++ src/main.py
@@ -9,3 +9,24 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 MAINNET_SUSHI_ADDRESS = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2"
+
+client = discord.Client(intents=discord.Intents.default())
+
+
+@tasks.loop(minutes=30)
+async def change_price_status():
+    sushi_price = fetch_token_prices({"mainnet": [MAINNET_SUSHI_ADDRESS]})["mainnet"][
+        MAINNET_SUSHI_ADDRESS
+    ]
+    await client.change_presence(
+        activity=discord.Game(name=f"Sushi Price: ${sushi_price:.2f}")
+    )
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+    change_price_status.start()
+
+
+client.run(TOKEN)
