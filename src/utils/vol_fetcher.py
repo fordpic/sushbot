6d002172a813f3e25cef94ea3c1bdd4e32468097
Ford Pickert <fordpickert@gmail.com>
2023-08-22 15:21:44 -0500

    token vol query first pass complete

--- src/utils/vol_fetcher.py
+++ src/utils/vol_fetcher.py
@@ -7,7 +7,7 @@ V2_EXCHANGE_ENDPOINTS = {
 
 
 def fetch_token_vol(token_addresses):
-    current_token_vol = """
+    current_token_vol_query = """
     query tokenVolumeQuery($tokenAddress: String!) {
         token(id: $tokenAddress) {
             volumeUSD
@@ -15,4 +15,18 @@ def fetch_token_vol(token_addresses):
         }
     }
 """
-    current_token_vol = {}
+    token_vols = {}
+
+    for chain in token_addresses:
+        token_vols[chain] = {}
+        for token in token_addresses:
+            variables = {"tokenAddress:", token}
+            result = requests.post(
+                V2_EXCHANGE_ENDPOINTS[chain],
+                json={"query": current_token_vol_query, "variables": variables},
+            )
+
+            data = json.loads(result.text)
+            token_vols[chain][token] = data["data"]["token"]["volumeUSD"]
+
+        return token_vols
